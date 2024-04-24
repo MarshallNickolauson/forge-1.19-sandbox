68a6ef80e1c59e1a12661fc2b95acf778f276bca
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-04-23 21:35:01 -0400

    blockstate example

--- src/main/java/net/marsh/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/marsh/tutorialmod/block/ModBlocks.java
@@ -2,6 +2,7 @@ package net.marsh.tutorialmod.block;
 
 import net.marsh.tutorialmod.TutorialMod;
 import net.marsh.tutorialmod.block.custom.JumpyBlock;
+import net.marsh.tutorialmod.block.custom.ZirconLampBlock;
 import net.marsh.tutorialmod.item.ModCreativeModeTab;
 import net.marsh.tutorialmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
@@ -38,6 +39,11 @@ public class ModBlocks {
     public static final RegistryObject<Block> JUMPY_BLOCK = registerBlock("jumpy_block",
             () -> new JumpyBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.TUTORIAL_TAB);
+
+    public static final RegistryObject<Block> ZIRCON_LAMP = registerBlock("zircon_lamp",
+            () -> new ZirconLampBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).requiresCorrectToolForDrops()
+                    .lightLevel(state -> state.getValue(ZirconLampBlock.LIT) ? 15 : 0)), ModCreativeModeTab.TUTORIAL_TAB);
 
 
 
